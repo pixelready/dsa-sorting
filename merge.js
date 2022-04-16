81bf9684f1e230cac457b6627ab85274a238854b
Owen Waring <bizartist@gmail.com>
2022-04-16 16:30:22 -0700

    WIP merge sort

--- merge.js
+++ merge.js
@@ -1,5 +1,15 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let length = arr1.length > arr2.length ? arr1.length : arr2.length;
+    let merged = [];
+    for (let i=0; i < length; i++){
+        if (arr1[i] > arr2[i]){
+            merged.push(arr1[i], arr2[i]);
+        }
+    }
+}
 
-function mergeSort() {}
+function mergeSort(arr) {
+
+}
 
 module.exports = { merge, mergeSort};
